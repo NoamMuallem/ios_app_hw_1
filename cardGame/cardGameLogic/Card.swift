ca879bc3db9a2f25422e59edede5cc57ba76929a
Noam Muallem <noammu072@gmail.com>
2022-05-15 02:24:01 +0300

    first commit - base logic and ui

---
+++ cardGame/cardGameLogic/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  cardGame
+//
+//  Created by noam muallem on 10/05/2022.
+//
+
+import Foundation
---
+++ cardGame/cardGameLogic/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  cardGame
+//
+//  Created by noam muallem on 10/05/2022.
+//
+
+import Foundation
